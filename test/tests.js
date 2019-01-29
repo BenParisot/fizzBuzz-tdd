ed053ed2c2b4c239f2cf2d787c9c793f183970d1
Ben Parisot <me@benparisot.com>
2019-01-29 11:06:37 -0800

    get tests.js working

--- test/tests.js
+++ test/tests.js
@@ -1,8 +1,29 @@
 const test = QUnit.test;
 
-test('time to write a test', function(assert) {
-    assert.equal('hi', 'hello');
+function even(number) {
+    if(number % 2 === 0) {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+
+test('return true if even', function(assert) {
+    const number = 10;
+    const expect = true;
+    const result = even(number);
+
+    assert.equal(result, expect);
+    
 });
 
+test('return false if off', function(assert){
+    const number = 3;
+    const expect = false;
+    const result = even(number);
+
+    assert.equal(result, expect);
+})
 // first parameter of test function is a string of what you want your test to do (or assert)
 // second parameter of test function is another function that asserts your test,, eg assert.what you want it to do
