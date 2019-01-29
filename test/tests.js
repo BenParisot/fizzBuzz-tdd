65e417b05be19649473b5554dc47db280cd1118d
Ben Parisot <me@benparisot.com>
2019-01-29 10:45:46 -0800

    start working with tests.js

--- test/tests.js
+++ test/tests.js
@@ -1,5 +1,8 @@
 const test = QUnit.test;
 
 test('time to write a test', function(assert) {
-    assert.equal(true, false);
+    assert.equal('hi', 'hello');
 });
+
+// first parameter of test function is a string of what you want your test to do (or assert)
+// second parameter of test function is another function that asserts your test,, eg assert.what you want it to do
